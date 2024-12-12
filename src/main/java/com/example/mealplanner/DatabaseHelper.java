7984eeb486aba079a7811102b1eb3993e4f83145
Sofielia <95904065+ainasofielia@users.noreply.github.com>
2024-12-12 16:09:31 +0800

    Update DatabaseHelper.java

--- src/main/java/com/example/mealplanner/DatabaseHelper.java
+++ src/main/java/com/example/mealplanner/DatabaseHelper.java
@@ -3,161 +3,80 @@ package com.example.mealplanner;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.database.DatabaseErrorHandler;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    // Database name and version
-    private static final String DATABASE_NAME = "mealPlanner.db";
+    private static final String DATABASE_NAME = "MealPlanner.db";
     private static final int DATABASE_VERSION = 1;
 
     // Table and column names
     private static final String TABLE_MEALS = "meals";
     private static final String COLUMN_ID = "id";
-    private static final String COLUMN_NAME = "name";
-    private static final String COLUMN_DESCRIPTION = "description";
-    private static final String COLUMN_DATE = "date";
-    // context
+    private static final String COLUMN_DAY = "day";
+    private static final String COLUMN_MEAL_NAME = "meal_name";
+    private static final String COLUMN_MEAL_DETAILS = "meal_details";
 
-
-
-    // SQL query to create table
-    private static final String CREATE_TABLE_MEALS = "CREATE TABLE " + TABLE_MEALS + " (" +
-            COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            COLUMN_NAME + " TEXT, " +
-            COLUMN_DESCRIPTION + " TEXT, " +
-            COLUMN_DATE + " TEXT);";
-
-    // Constructor
-    public DatabaseHelper(@Nullable Context context) {
+    public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // Create tables when database is first created
-        db.execSQL(CREATE_TABLE_MEALS);
+        String createTable = "CREATE TABLE " + TABLE_MEALS + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_DAY + " TEXT, " +
+                COLUMN_MEAL_NAME + " TEXT, " +
+                COLUMN_MEAL_DETAILS + " TEXT)";
+        db.execSQL(createTable);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // Drop older tables if they exist
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_MEALS);
-        // Create tables again
         onCreate(db);
     }
 
-    // Method to add a meal
-    public long addMeal(String name, String description, String date) {
+    // Insert meal
+    public boolean insertMeal(String day, String mealName, String mealDetails) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
-        values.put(COLUMN_NAME, name);
-        values.put(COLUMN_DESCRIPTION, description);
-        values.put(COLUMN_DATE, date);
+        values.put(COLUMN_DAY, day);
+        values.put(COLUMN_MEAL_NAME, mealName);
+        values.put(COLUMN_MEAL_DETAILS, mealDetails);
 
         long result = db.insert(TABLE_MEALS, null, values);
-        db.close();
-        return result;
+        return result != -1; // Return true if insert is successful
     }
-    // Method to get all meals
-    public List<Meal> getAllMeals() {
-        List<Meal> meals = new ArrayList<>();
+
+    // Retrieve all meals for a specific day
+    public List<String> getMealsByDay(String day) {
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.query(TABLE_MEALS, null, null, null, null, null, null);
+        List<String> meals = new ArrayList<>();
+        Cursor cursor = db.query(TABLE_MEALS,
+                new String[]{COLUMN_MEAL_NAME, COLUMN_MEAL_DETAILS},
+                COLUMN_DAY + "=?",
+                new String[]{day},
+                null, null, null);
 
-        if (cursor != null) {
-            while (cursor.moveToNext()) {
-                int id = cursor.getInt(cursor.getColumnIndex(COLUMN_ID));
-                String name = cursor.getString(cursor.getColumnIndex(COLUMN_NAME));
-                String description = cursor.getString(cursor.getColumnIndex(COLUMN_DESCRIPTION));
-                String date = cursor.getString(cursor.getColumnIndex(COLUMN_DATE));
-
-                meals.add(new Meal(id, name, description, date));
-            }
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                String meal = cursor.getString(0) + ": " + cursor.getString(1);
+                meals.add(meal);
+            } while (cursor.moveToNext());
             cursor.close();
         }
-
-        db.close();
         return meals;
     }
 
-    // Method to get a specific meal by ID
-    public Meal getMealById(int mealId) {
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.query(TABLE_MEALS, null, COLUMN_ID + "=?", new String[]{String.valueOf(mealId)}, null, null, null);
-
-        if (cursor != null) {
-            if (cursor.moveToFirst()) {
-                int id = cursor.getInt(cursor.getColumnIndex(COLUMN_ID));
-                String name = cursor.getString(cursor.getColumnIndex(COLUMN_NAME));
-                String description = cursor.getString(cursor.getColumnIndex(COLUMN_DESCRIPTION));
-                String date = cursor.getString(cursor.getColumnIndex(COLUMN_DATE));
-
-                cursor.close();
-                db.close();
-                return new Meal(id, name, description, date);
-            }
-        }
-        db.close();
-        return null;
-    }
-
-    // Method to update a meal
-    public int updateMeal(int id, String name, String description, String date) {
+    // Clear all meals for testing or refreshing
+    public void clearMeals() {
         SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(COLUMN_NAME, name);
-        values.put(COLUMN_DESCRIPTION, description);
-        values.put(COLUMN_DATE, date);
-
-        int result = db.update(TABLE_MEALS, values, COLUMN_ID + "=?", new String[]{String.valueOf(id)});
-        db.close();
-        return result;
-    }
-    // Method to delete a meal
-    public void deleteMeal(int id) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(TABLE_MEALS, COLUMN_ID + "=?", new String[]{String.valueOf(id)});
-        db.close();
-    }
-
-    // Model class for Meal
-    public static class Meal {
-        private int id;
-        private String name;
-        private String description;
-        private String date;
-
-        public Meal(int id, String name, String description, String date) {
-            this.id = id;
-            this.name = name;
-            this.description = description;
-            this.date = date;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public String getDate() {
-            return date;
-        }
+        db.execSQL("DELETE FROM " + TABLE_MEALS);
     }
 }
